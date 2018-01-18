eae77b15635abafd8b941347441ddd8d847ebbae
Kirill Kirikov <cto@seductive.com.ua>
2018-01-18 16:54:55 +0800

    fixed test

--- test/m8badge.js
+++ test/m8badge.js
@@ -1,5 +1,9 @@
 var M8BadgeToken = artifacts.require("./M8BadgeToken.sol");
 
 contract('M8BadgeToken', function (accounts) {
-    M8BadgeToken.deployed().then(instance => { return instance.ownerOf(accounts[0]); }).then( value => { return value.toNumber() });
+    M8BadgeToken.deployed().then(instance => {
+        return instance.ownerOf(0);
+    }).then( address => {
+        assertEqual(address, accounts[0], "Owner of 0 (Genesis) badge should be the contract owner.")
+    });
 });
